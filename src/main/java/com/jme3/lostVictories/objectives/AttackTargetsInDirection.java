a3f30090397ad33459a712515c32118dfd46c95b
darthShana <darthShana@gmail.com>
2018-05-04 08:33:39 +1200

    attack multiple targets

--- src/main/java/com/jme3/lostVictories/objectives/AttackTargetsInDirection.java
+++ src/main/java/com/jme3/lostVictories/objectives/AttackTargetsInDirection.java
@@ -7,18 +7,19 @@ import lostVictories.dao.HouseDAO;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 public class AttackTargetsInDirection extends Objective{
 
-    Vector lastKnownLocation;
+    Set<Vector> lastKnownLocations;
     TravelObjective travelObjective;
 
 
     @Override
     public void runObjective(CharacterMessage c, String uuid, CharacterDAO characterDAO, HouseDAO houseDAO, Map<UUID, CharacterMessage> toSave, Map<UUID, UUID> kills) {
-        if(travelObjective == null){
-            travelObjective = new TravelObjective(c, lastKnownLocation, null);
+        if(travelObjective == null && !lastKnownLocations.isEmpty()){
+            travelObjective = new TravelObjective(c, lastKnownLocations.iterator().next(), null);
         }
         if(travelObjective!=null && !travelObjective.isComplete){
             travelObjective.runObjective(c, uuid, characterDAO, houseDAO, toSave, kills);
